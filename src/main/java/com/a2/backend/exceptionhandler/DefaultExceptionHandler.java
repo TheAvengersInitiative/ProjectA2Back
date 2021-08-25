2fe58305aa32ad58fb37e46d3df3e69de37091ea
manumasjoan <56048561+manumasjoan@users.noreply.github.com>
2021-08-25 13:00:53 -0300

    Us 3.1.1/alta de proyecto (#3)
    * started with create new project
    
    * added transient
    
    * Added tests and corrections
    
    * Added tests and corrections
    
    Co-authored-by: Manuela Masjoan <manuelamasjoan@MacBook-Air-de-Manuela.local>

--- src/main/java/com/a2/backend/exceptionhandler/DefaultExceptionHandler.java
+++ src/main/java/com/a2/backend/exceptionhandler/DefaultExceptionHandler.java
@@ -5,14 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class DefaultExceptionHandler {
@@ -20,23 +17,16 @@ public class DefaultExceptionHandler {
     Logger logger = LoggerFactory.getLogger(DefaultExceptionHandler.class);
 
     @ExceptionHandler(ProjectWithThatTitleExistsException.class)
-    protected ResponseEntity<Object> handleProjectWithThatTitleExists(ProjectWithThatTitleExistsException exception) {
+    protected ResponseEntity<?> handleProjectWithThatTitleExists(ProjectWithThatTitleExistsException exception) {
+        logger.info(exception.getMessage());
+        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException exception) {
         logger.info(exception.getMessage());
         return ResponseEntity.badRequest().build();
     }
-
-    /** ignore
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }**/
 
 }
---
+++ src/test/java/com/a2/backend/controller/ProjectControllerTest.java
@@ -0,0 +1,101 @@
+package com.a2.backend.controller;
+
+import com.a2.backend.entity.Project;
+import com.a2.backend.exception.ProjectWithThatTitleExistsException;
+import com.a2.backend.model.ProjectCreateDTO;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class ProjectControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private final String baseUrl = "/project";
+
+    @Test
+    void Test001_ProjectControllerWhenReceivesValidCreateProjectDTOShouldReturnStatusCreated() {
+        String title = "Project title";
+        String  description = "Testing exception for existing title";
+        String owner = "Owner´s name";
+
+        ProjectCreateDTO projectToCreate = ProjectCreateDTO.builder()
+                .title(title)
+                .description(description)
+                .owner(owner)
+                .build();
+
+        HttpEntity<ProjectCreateDTO> request = new HttpEntity<>(projectToCreate);
+
+        val getResponse = restTemplate.exchange(baseUrl, HttpMethod.POST, request, Project.class);
+        assertEquals(HttpStatus.CREATED, getResponse.getStatusCode());
+    }
+
+    @Test
+    void Test002_ProjectControllerWhenReceiveCreateProjectDTOWithInvalidTitleShouldReturnStatusBadRequest() {
+        String title = "a";
+        String  description = "Testing exception for existing title";
+        String owner = "Owner´s name";
+
+        ProjectCreateDTO projectToCreate = ProjectCreateDTO.builder()
+                .title(title)
+                .description(description)
+                .owner(owner)
+                .build();
+
+        HttpEntity<ProjectCreateDTO> request = new HttpEntity<>(projectToCreate);
+
+        val getResponse = restTemplate.exchange(baseUrl, HttpMethod.POST, request, Project.class);
+        assertEquals(HttpStatus.BAD_REQUEST, getResponse.getStatusCode());
+    }
+
+    @Test
+    void Test003_ProjectControllerWhenReceiveCreateProjectDTOWithInvalidDescriptionShouldReturnStatusBadRequest() {
+        String title = "Project title";
+        String  description = "Short";
+        String owner = "Owner´s name";
+
+        ProjectCreateDTO projectToCreate = ProjectCreateDTO.builder()
+                .title(title)
+                .description(description)
+                .owner(owner)
+                .build();
+
+        HttpEntity<ProjectCreateDTO> request = new HttpEntity<>(projectToCreate);
+
+        val getResponse = restTemplate.exchange(baseUrl, HttpMethod.POST, request, Project.class);
+        assertEquals(HttpStatus.BAD_REQUEST, getResponse.getStatusCode());
+    }
+
+    @Test
+    void Test004_ProjectControllerWhenReceiveCreateProjectDTOWithInvalidDescriptionAndTitleShouldReturnStatusBadRequest() {
+        String title = "a";
+        String  description = "Short";
+        String owner = "Owner´s name";
+
+        ProjectCreateDTO projectToCreate = ProjectCreateDTO.builder()
+                .title(title)
+                .description(description)
+                .owner(owner)
+                .build();
+
+        HttpEntity<ProjectCreateDTO> request = new HttpEntity<>(projectToCreate);
+
+        val getResponse = restTemplate.exchange(baseUrl, HttpMethod.POST, request, Project.class);
+        assertEquals(HttpStatus.BAD_REQUEST, getResponse.getStatusCode());
+    }
+
+}
