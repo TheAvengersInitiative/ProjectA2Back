a25810d1f53cc1eed06d71bea9b520b33f39655e
juanlopezcolombo <juan.lopez@ing.austral.edu.ar>
2021-08-25 07:32:55 -0300

    Baja de proyecto, Tests fueron agregados `1` tests agregados

---
+++ src/main/java/com/a2/backend/exception/ProjectWithThatIdDoesntExistException.java
@@ -0,0 +1,7 @@
+package com.a2.backend.exception;
+
+public class ProjectWithThatIdDoesntExistException extends RuntimeException{
+    public ProjectWithThatIdDoesntExistException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/a2/backend/exceptionhandler/DefaultExceptionHandler.java
+++ src/main/java/com/a2/backend/exceptionhandler/DefaultExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.a2.backend.exceptionhandler;
 
+import com.a2.backend.exception.ProjectWithThatIdDoesntExistException;
 import com.a2.backend.exception.ProjectWithThatTitleExistsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,11 +23,9 @@ public class DefaultExceptionHandler {
         return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);    }
 
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException exception) {
+
+    @ExceptionHandler(ProjectWithThatIdDoesntExistException.class)
+    protected ResponseEntity<Object> handleProjectWithThatIdDoesntExist(ProjectWithThatIdDoesntExistException exception) {
         logger.info(exception.getMessage());
         return ResponseEntity.badRequest().build();
-    }
-
-}
+    }}
