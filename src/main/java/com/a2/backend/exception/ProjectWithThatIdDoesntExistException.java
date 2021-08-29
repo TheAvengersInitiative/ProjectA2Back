a39984494385e7b9013b6a304f5474e78c4850dc
juanlopezcolombo <juan.lopez@ing.austral.edu.ar>
2021-08-29 10:46:22 -0300

    Eliminación de lógica y excepción

--- src/main/java/com/a2/backend/exception/ProjectWithThatIdDoesntExistException.java
+++
@@ -1,7 +0,0 @@
-package com.a2.backend.exception;
-
-public class ProjectWithThatIdDoesntExistException extends RuntimeException {
-    public ProjectWithThatIdDoesntExistException(String message) {
-        super(message);
-    }
-}
