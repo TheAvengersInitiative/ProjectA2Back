38221e4e5177e9eb74d5d7ea938b9313a33957ae
agusayerza <agusayerza@gmail.com>
2021-08-30 22:34:05 -0300

    Demo runner (#20)

---
+++ src/main/java/com/a2/backend/annotation/Generated.java
@@ -0,0 +1,8 @@
+package com.a2.backend.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Generated {}
