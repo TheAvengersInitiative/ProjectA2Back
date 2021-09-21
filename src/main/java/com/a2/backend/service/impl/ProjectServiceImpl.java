520caf8d439bd7d5582108a95c5aa48e9f136496
juanlopezcolombo <juan.lopez@ing.austral.edu.ar>
2021-09-21 12:30:07 -0300

    Search by links and Tags incorporated

--- src/main/java/com/a2/backend/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/a2/backend/service/impl/ProjectServiceImpl.java
@@ -188,10 +188,11 @@ public class ProjectServiceImpl implements ProjectService {
         for (String tag : tags) {
             result.addAll(projectRepository.findProjectsByTagName(tag));
         }
-        for (int i = 0; i < result.size(); i++) {
-            for (int j = 0; j < result.size(); j++) {
+        for (int i = 0; i < result.size() - 1; i++) {
+            for (int j = i + 1; j < result.size(); j++) {
                 if (result.get(i).getId().equals(result.get(j).getId())) {
                     result.remove(i);
+                    i--;
                     break;
                 }
             }
