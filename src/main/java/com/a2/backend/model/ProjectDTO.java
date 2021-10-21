e2f7ea4f86387281506cada689df7f0b5f7ddb35
juanlopezcolombo <juan.lopez@ing.austral.edu.ar>
2021-10-21 19:38:50 -0300

    added discussion to project dto

--- src/main/java/com/a2/backend/model/ProjectDTO.java
+++ src/main/java/com/a2/backend/model/ProjectDTO.java
@@ -1,5 +1,6 @@
 package com.a2.backend.model;
 
+import com.a2.backend.entity.Discussion;
 import com.a2.backend.entity.ForumTag;
 import com.a2.backend.entity.Language;
 import com.a2.backend.entity.Tag;
